0dce2d569a8f807de4309ebfb09cf26f225d4d40
Yared Santiago <yaredmyers@gmail.com>
2019-01-10 12:48:01 +0100

    DB connected

--- app.js
+++ app.js
@@ -3,7 +3,19 @@ const app = express();
 const bodyParser = require("body-parser");
 const modulePost = require('./routes/modulePos');
 const index = require('./routes/index');
+const mongoose = require('mongoose');
+// const Cabi = require('./model/Cabi')
 
+setTimeout(function(){
+  mongoose
+      .connect('mongodb://yared_gonzalez_mongo_1/cabiDB', { useNewUrlParser: true })
+      .then(x => {
+          console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+      })
+      .catch(err => {
+          console.error('Error connecting to mongo', err)
+      });
+}, 10000);
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
