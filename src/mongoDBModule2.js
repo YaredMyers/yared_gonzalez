b6fd3e09b430538ad1b27efa67f8364849604ccc
Yared Santiago <yaredmyers@gmail.com>
2019-01-15 18:08:08 +0100

    start DB validations

--- src/mongoDBModule2.js
+++ src/mongoDBModule2.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 //prueba
 
 // var conn      = mongoose.createConnection(process.env.CabiDB, { useNewUrlParser: true });
@@ -18,17 +18,7 @@ require('dotenv').config();
 //     }, 10000);
 // }
 
-
 // var CabiRepCredit = conn2.model("CabiGlobalCredit", CabiGlobalCredit)
-
-// let db = {
-//   conn: {
-//     leader: true
-//   },
-//   conn2: {
-//     leader: false
-//   }
-// }
 
 // // stored in 'testA' database
 // var myCabiDB    = conn.model('Model', new mongoose.Schema({
@@ -42,23 +32,51 @@ require('dotenv').config();
 
 // module.exports = {conn,conn2};
 
-
-
 // require('dotenv').config();
 // const mongoose = require('mongoose');
 const conn = mongoose.createConnection(process.env.CabiDB, {
-   useNewUrlParser: true 
-})
-const conn2 = mongoose.createConnection(process.env.CabiDB2, {
-   useNewUrlParser: true  
+  useNewUrlParser: true
 });
+const conn2 = mongoose.createConnection(process.env.CabiDB2, {
+  useNewUrlParser: true
+});
+
+let db = {
+  conn: {
+    isPrimary: true,
+    conn: conn
+  },
+  conn2: {
+    isPrimary: false,
+    conn: conn2
+  }
+};
+
+function getConnection(type) {
+  if (type === "isPrimary") {
+    return conn.isPrimary ? conn.conn : conn2.conn; 
+  } else if (type === "replica") {
+    return 
+  }
+}
+
+// let validateDB = function(){
+//   if (db.conn === true){
+//     return conn;
+//   } else {
+//     return conn2;
+//   }
+// }
+
+
+
 // let mongoConnect = function (param) {
 //   const clear = setInterval(() => {
 //      mongoose.Promise = Promise;
 //      mongoose
 //        .createConnection(param, {
 //           useNewUrlParser: true
-          
+
 //        })
 //        .then(x => {
 //           console.log(`Connected to Mongo! Database name: "${x.name}"`)
