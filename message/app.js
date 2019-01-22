16c245bdd74255477cd5af2251228c68f1b80bf0
Yared Santiago <yaredmyers@gmail.com>
2019-01-22 15:05:56 +0100

    starting division credit-message folders

---
+++ message/app.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const bodyParser = require("body-parser");
+const modulePost = require('./src/routes/modulePos');
+const index = require('./src/routes/modulePos');
+const getAllMsgRoute = require('./src/client/msgGet');
+const creditPost = require('./src/routes/creditPost');
+const myPORT = process.env.PORT;
+const getCreditRoute = require('./src/client/creditGet')
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// app.use('/', index);
+app.use('/', modulePost);
+app.use('/', getAllMsgRoute);
+app.use('/', creditPost);
+app.use('/', getCreditRoute);
+
+
+// Server Started
+app.listen(9001, () => {
+  console.log('Hola, Mundo!')
+});
+
+// app.listen(myPORT, () => {
+// console.log('Hello World! From 9001 PORT!!')
+// });
---
+++ message/mongoDBMessage.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const conn = mongoose.createConnection(process.env.CabiDB, {
+  useNewUrlParser: true
+});
+const conn2 = mongoose.createConnection(process.env.CabiDB2, {
+  useNewUrlParser: true
+});
+
+let db = {
+  conn: {
+    isPrimary: true,
+    mongo: conn
+  },
+  conn2: {
+    isPrimary: false,
+    mongo: conn2
+  }
+};
+
+let getConnection = function(type) {
+  if (type === "primary") {
+    return db.conn.isPrimary && db.conn.mongo.readyState === 1
+      ? db.conn.mongo
+      : db.conn2.mongo;
+  } else if (type === "replica") {
+    return db.conn.isPrimary && db.conn2.mongo.readyState === 1
+      ? db.conn2.mongo
+      : db.conn.mongo;
+  }
+};
+
+let isReplicaOnline = function() {
+  if (db.conn.mongo.readyState === 1 && db.conn2.mongo.readyState === 1) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+module.exports = { getConnection, isReplicaOnline, db };
