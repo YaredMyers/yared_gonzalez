2495b41c784626f3f331894183148e13e4c366ec
Yared Santiago <yaredmyers@gmail.com>
2019-01-25 14:39:30 +0100

    ex 10 iteration2

--- message/src/messageAppAxios/clientMessageApp.js
+++ message/src/messageAppAxios/clientMessageApp.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const axiosMessageApp = process.env.AxiosREQ;
 
 let clientMessageApp = function(destination, body) {
-  return axios.post(axiosMessageApp, { destination, body }, { timeout: 8000 })
+  return axios.post(axiosMessageApp, { destination, body }, { timeout: 10000 })
   .then((resp)=> {
     // console.log(resp)
     throw ("OK");
